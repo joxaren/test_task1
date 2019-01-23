b98ea81b12688f86502ac69f36be764ee17d0a5c
Королюк Алена <korolyuk@msk.i-teco.ru>
2019-01-23 11:25:27 +0300

    redacted

--- src/main/java/POServlet.java
+++ src/main/java/POServlet.java
@@ -17,27 +17,7 @@ public class POServlet extends HttpServlet {
     }
 
     private void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        String title = "HTTP Header Request Example";
-        String docType = "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n";
-        out.println(docType +
-                "<html>\n" +
-                "<head><title>" + title + "</title></head>\n"+
-                "<body bgcolor = \"#f0f0f0\">\n" +
-                "<h1 align = \"center\">" + title + "</h1>\n" +
-                "<table width = \"100px\" border = \"1\" align = \"center\">\n" +
-                "<tr bgcolor = \"#949494\">\n" +
-                "<th>Header Name</th><th>Header Value(s)</th>\n"+
-                "</tr>\n");
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while(headerNames.hasMoreElements()) {
-            String paramName = (String)headerNames.nextElement();
-            out.print("<tr><td>" + paramName + "</td>\n");
-            String paramValue = request.getHeader(paramName);
-            out.println("<td> " + paramValue + "</td></tr>\n");
-        }
-        out.println("</table>\n</body></html>");
+        
     }
 
 
