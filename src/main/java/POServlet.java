e81551323db2e845e9668b936970fb18b1007ba5
Королюк Алена <korolyuk@msk.i-teco.ru>
2019-01-23 14:47:03 +0300

    more changes

--- src/main/java/POServlet.java
+++ src/main/java/POServlet.java
@@ -1,25 +1,80 @@
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Enumeration;
+import java.io.*;
+
+import static java.time.LocalTime.now;
 
 public class POServlet extends HttpServlet {
 
-    int SC_CUSTOM_TEST = 1;
+    private int SC_CUSTOM_TEST;
 
     public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
             throws IOException {
-        httpServletResponse.getWriter().print("test");
-        try{
+        try {
             handleRequest(httpServletRequest, httpServletResponse);
-        } catch (Exception ex) {}
+        } catch (Exception ex) {
+        }
     }
 
     private void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        String IDNum = requestBodyToString(request);
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        if (IDNum != null && !IDNum.isEmpty()) {
+            SC_CUSTOM_TEST = 0;
+        } else {
+            SC_CUSTOM_TEST = -1;
+        }
+        out.println("<!DOCTYPE html>");
+        out.println("<body>");
+        out.println("<p>IDNum: " + IDNum + "</p>");
+        out.println("<p>Status: " + SC_CUSTOM_TEST + "</p>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
     }
 
+    private static String requestBodyToString(HttpServletRequest request) {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+        try {
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(
+                        inputStream));
+                char[] charBuffer = new char[256];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            } else {
+                stringBuilder.append("");
+            }
+        } catch (IOException ex) {
+            logMessage("requestBodyToString", "Got error:" + ex.toString());
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException ex) {
+                    logMessage("requestBodyToString", "Got error:" + ex.toString());
+                }
+            }
+        }
+        String body = stringBuilder.toString();
+        return body;
+    }
 
+    private static void logMessage(String module, String message) {
+        System.out.println(now() + "\t " + module + ":" + message);
+    }
 
+    private static String makeSoapResponseSimple(String InnerXml) {
+        return "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"><soap:Body>" + InnerXml + "</soap:Body></soap:Envelope>";
+    }
 }
